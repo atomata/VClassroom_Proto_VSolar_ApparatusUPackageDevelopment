03c9d08d3bf7207d4b09cbeb9a12c573d9ea14a0
james <jamesmccafferty@live.ca>
2021-10-16 14:56:19 -0400

    Logs folder did not push correctly

---
+++ Assets/Dependencies/Hex/UN/Runtime/Scripts/Framework/Debugging/Logging/SOHexLog.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+using UnityEngine;
+
+namespace HexUN.Framework.Debugging
+{
+    [CreateAssetMenu(fileName = "HexLog", menuName = "HexUN/Services/HexLog")]
+    public class SoHexLog : ScriptableObject, ILog
+    {
+        private const string cOpenBrack = "[";
+        private const string cCloseBrack = "]";
+        private const string cSep = " - ";
+
+        private Action<string> _logInfoAction = Debug.Log;
+        private Action<string> _logWarningAction = Debug.LogWarning;
+        private Action<string> _logErrorAction = Debug.LogError;
+
+        private StringBuilder _sb = new StringBuilder();
+
+        /// <inheritdoc />
+        public Action<string> LogInfoAction { get => _logInfoAction; set => _logInfoAction = value; }
+
+        /// <inheritdoc />
+        public Action<string> LogWarnAction { get => _logWarningAction; set => _logWarningAction = value; }
+
+        /// <inheritdoc />
+        public Action<string> LogErrorAction { get => _logErrorAction; set => _logErrorAction = value; }
+
+
+        #region API
+        /// <inheritdoc />
+        public void Error(string category, string message, bool forUser = false)
+        {
+            string log = WriteLog(category, message);
+            LogErrorAction(log);
+            PushLog(log);
+        }
+
+        /// <inheritdoc />
+        public void Error(string category, string message, Exception e, bool forUser = false)
+        {
+            string log = WriteLog(category, $"{message}\n Exception: {e.Message}\nStack Trace:\n {e.StackTrace}");
+            LogErrorAction(log);
+            PushLog(log);
+        }
+
+        /// <inheritdoc />
+        public void Info(string category, string message, bool forUser = false)
+        {
+            string log = WriteLog(category, message);
+            LogInfoAction(log);
+            PushLog(log);
+        }
+
+        /// <inheritdoc />
+        public void Warn(string category, string message, bool forUser = false)
+        {
+            string log = WriteLog(category, message);
+            LogWarnAction(log);
+            PushLog(log);
+        }
+        #endregion
+
+        private string WriteLog(string category, string message)
+        {
+            _sb.Clear();
+            _sb.Append(cOpenBrack);
+            _sb.Append(category);
+            _sb.Append(cCloseBrack);
+            _sb.Append(cSep);
+            _sb.AppendLine(message);
+            return _sb.ToString();
+        }
+
+        private void PushLog(string log)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Dependencies/Hex/UN/Runtime/Scripts/Framework/Debugging/Logging/UTILog.cs
@@ -0,0 +1,18 @@
+﻿namespace HexUN.Framework.Debugging
+{
+    /// <summary>
+    /// Useful functions that operate on ILog
+    /// </summary>
+    public static class UtILog
+    {
+        public static void Error_NullArgument<TType>(this ILog log, string category, string argName) where TType : class
+        {
+            log.Error(category, $"{nameof(TType)} {argName} is null");
+        }
+
+        public static void Error_PrefabLoad(this ILog log, string category, string prefabName)
+        {
+            log.Error(category, $"Failed to load prefab {prefabName}");
+        }
+    }
+}
