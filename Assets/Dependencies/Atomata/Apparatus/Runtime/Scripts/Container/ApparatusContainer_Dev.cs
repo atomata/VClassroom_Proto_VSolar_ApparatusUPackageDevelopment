e14c2962dffb019afe96b213f4f43ebb98728b03
james <jamesmccafferty@live.ca>
2021-10-24 16:49:23 -0400

    Added some logs to the development container

--- Assets/Dependencies/Atomata/Apparatus/Runtime/Scripts/Container/ApparatusContainer_Dev.cs
+++ Assets/Dependencies/Atomata/Apparatus/Runtime/Scripts/Container/ApparatusContainer_Dev.cs
@@ -2,6 +2,8 @@ using Atomata.VSolar.Apparatus.UnityEditor;
 
 using Cysharp.Threading.Tasks;
 
+using HexUN.Framework;
+
 using UnityEngine;
 
 namespace Atomata.VSolar.Apparatus {
@@ -11,6 +13,8 @@ namespace Atomata.VSolar.Apparatus {
     [ExecuteAlways]
     public class ApparatusContainer_Dev : MonoBehaviour
     {
+        private const string cLogCategory = nameof(ApparatusContainer_Dev);
+
         [SerializeField]
         private SoApparatusConfig Config;
 
@@ -19,17 +23,27 @@ namespace Atomata.VSolar.Apparatus {
 
         public async void OnEnable()
         {
-            if (Node == null) return;
+            if (Node == null)
+            {
+                OneHexServices.Instance.Log.Warn(cLogCategory, $"Cannot initalize {nameof(SerializationNode)} because node is null", false);
+                return;
+            }
+
+            OneHexServices.Instance.Log.Info(cLogCategory, "Initializing managed serialization node by Unloading, Disconnecting, Connecting and Reloading", false);
 
             await Node.Trigger(ApparatusTrigger.LoadTrigger(false));
             Node.Disconnect();
 
             Node.Connect();
             await Node.Trigger(ApparatusTrigger.LoadTrigger(true));
+
+            OneHexServices.Instance.Log.Info(cLogCategory, "Initializing managed serialization node complete", false);
+
         }
 
         public void HandleRequest(ApparatusRequest req)
         {
+            OneHexServices.Instance.Log.Info(cLogCategory, $"Received request and handling", false);
             Config.Node_OnRequest(req);
         }
     }
