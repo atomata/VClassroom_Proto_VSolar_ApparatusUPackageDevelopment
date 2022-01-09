ec95a3b545f189d839fc8c8c99aa0813668caed2
james mccafferty <james.mccafferty@unity3d.com>
2022-01-09 11:47:36 -0500

    Cleaned up the AtomataSceneManager

---
+++ Assets/Dependencies/Atomata/Apparatus/Runtime/Scripts/AtomataScene/AtomataSceneDebugger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Atomata.VSolar.Apparatus;
+using UnityEngine;
+
+public class AtomataSceneDebugger : MonoBehaviour
+{
+    public AtomataSceneManager manager;
+    
+    public string Input;
+    
+    [ContextMenu("LoadApparatus")] void LoadApparatus() => manager.LoadApparatus(Input);
+    [ContextMenu("UnloadApparatus")] void UnloadApparatus() => manager.UnloadApparatus();
+    [ContextMenu("LoadSkybox")] void LoadSkybox() => manager.LoadSkybox(Input);
+    [ContextMenu("Trigger")] void Trigger() => manager.Trigger(Input);
+}
